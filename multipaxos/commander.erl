0e69ef30df08bf2f0e4c73bac56e62142d337a05
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-07 14:28:23 +0000

    Add commander

---
+++ multipaxos/commander.erl
@@ -0,0 +1,25 @@
+%%% Frederick Lindsey (fl1414) and Cyrus Vahidi (cv114)
+
+-module(commander).
+-export([start/6]).
+
+start(Leader, Acceptors, Replicas, Ballot, Slot, Cmd) ->
+  WaitFor = sets:from_list(Acceptors),
+  [ P ! { commander2acceptor, self(), Ballot, Slot, Cmd } || P <- WaitFor ],
+  next(Leader, Replicas, Ballot, Slot, Cmd, WaitFor, sets:size(WaitFor)).
+
+next(Leader, Replicas, Ballot, Slot, Cmd, WaitFor, Size) ->
+  receive
+    { acceptor2commander, PID, Ballot, Slot, Cmd } ->
+      case (sets:is_element(PID, WaitFor)) of
+        true  ->
+          WaitForN  = sets:remove_element(PID),
+          case (sets:size(WaitForN) < (Size / 2)) of
+            true  -> [ R ! { decision, self(), Slot, Cmd } || R <- Replicas ];
+            false -> next(Leader, Replicas, Ballot, Slot, Cmd, WaitForN, Size)
+          end ;
+        false ->
+          Leader ! { preempted, self(), Ballot }
+      end
+  end,
+  done.
