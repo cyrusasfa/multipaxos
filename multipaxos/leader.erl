c85674af1732d1ab9e852a678049aeaf0aa34bb4
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-07 15:32:36 +0000

    Initial implementation of leader

--- multipaxos/leader.erl
+++ multipaxos/leader.erl
@@ -4,4 +4,54 @@
 -export([start/0]).
 
 start() ->
-  io:format('LEADER').
+  receive
+    { bind, Acceptors, Replicas } ->
+      spawn(scout, start, [self(), Acceptors, 0]),
+      next(false, 0, maps:new(), Acceptors, Replicas)
+  end.
+
+next(Active, Ballot, Proposals, Acceptors, Replicas) ->
+  receive
+    { propose, Slot, Cmd } ->
+      case maps:is_key(Slot, Proposals) of
+        false ->
+          ProposalsN = maps:put(Slot, Cmd, Proposals),
+          case Active of
+            true  ->
+              spawn(commander, start,
+                    [self(), Acceptors, Replicas, Ballot, Slot, Cmd])
+          end,
+          next(Active, Ballot, ProposalsN, Acceptors, Replicas)
+      end ;
+    { adopted, _, Ballot, Accepted } ->
+      Commands = accumulate_proposals(Accepted, maps:new(), maps:new()),
+      [ spawn(
+          commander, start,
+          [self(), Acceptors, Replicas, Ballot, Slot, C ]
+        ) || { Slot, C } <- maps:to_list(Commands) ],
+      next(true, Ballot, Proposals, Acceptors, Replicas) ;
+    { preempted, _, BallotN } ->
+      case BallotN > Ballot of
+        true  ->
+          BallotNN = Ballot + 1,
+          spawn(scout, start, [self(), Acceptors, BallotNN]) ;
+        false ->
+          BallotNN = Ballot
+      end,
+      next(false, BallotNN, Proposals, Acceptors, Replicas)
+  end.
+
+accumulate_proposals(Proposals, Commands, Map) ->
+  case Proposals of
+    [ { Ballot, Slot, Cmd } | T ] ->
+      case (not maps:is_key(Slot, Map) or (maps:get(Slot, Map) < Ballot)) of
+        true  ->
+          accumulate_proposals(
+            T,
+            maps:put(Slot, Cmd, Commands),
+            maps:put(Slot, Ballot, Map)
+          ) ;
+        false -> accumulate_proposals(T, Commands, Map)
+      end ;
+    _ -> Commands
+  end.
