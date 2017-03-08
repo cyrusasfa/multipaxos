a754bac7b9d8ab7707f782116ddb513e1c26d6da
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-08 13:17:03 +0000

    Comments

--- multipaxos/acceptor.erl
+++ multipaxos/acceptor.erl
@@ -11,13 +11,16 @@ start() ->
 % Accepted = [ PValue ]
 next(Ballot, Accepted) ->
   receive
-    { phase1, request, Scout, BallotI} ->
-      case (BallotI> Ballot) of
+    % Adopt a ballot number
+    { phase1, request, Scout, BallotI } ->
+      case (BallotI > Ballot) of
         true  -> BallotO = BallotI;
         false -> BallotO = Ballot
       end,
       Scout ! { phase1, response, self(), BallotO, Accepted },
       AcceptedO = Accepted ;
+
+    % Adopt a pvalue
     { phase2, request, Commander, BallotI, SlotI, Cmd } ->
       case (BallotI == Ballot) of
         true  ->
