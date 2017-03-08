6ea68d6e10c9668b9f36a57c41147f8276cf6a16
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-08 13:24:02 +0000

    Improvements to commander

--- multipaxos/commander.erl
+++ multipaxos/commander.erl
@@ -8,15 +8,18 @@ start(Leader, Acceptors, Replicas, Ballot, Slot, Cmd) ->
   [ P ! { phase2, request, self(), Ballot, Slot, Cmd } || P <- WaitFor ],
   next(Leader, Replicas, Ballot, Slot, Cmd, WaitFor, sets:size(WaitFor)).
 
+% Replicas = [ PID ]
+% Ballot = { Round, Leader }
+% WaitFor = [ PID ]
 next(Leader, Replicas, Ballot, Slot, Cmd, WaitFor, Size) ->
   receive
-    { phase2, response, PID, Ballot, Slot, Cmd } ->
-      case (sets:is_element(PID, WaitFor)) of
+    { phase2, response, PID, BallotI, Slot, Cmd } ->
+      case (BallotI == Ballot andalso sets:is_element(PID, WaitFor)) of
         true  ->
-          WaitForN  = sets:remove_element(PID),
-          case (sets:size(WaitForN) < (Size / 2)) of
+          WaitForO  = sets:remove_element(PID),
+          case (sets:size(WaitForO) < (Size / 2)) of
             true  -> [ R ! { decision, self(), Slot, Cmd } || R <- Replicas ];
-            false -> next(Leader, Replicas, Ballot, Slot, Cmd, WaitForN, Size)
+            false -> next(Leader, Replicas, Ballot, Slot, Cmd, WaitForO, Size)
           end ;
         false ->
           Leader ! { preempted, self(), Ballot }
